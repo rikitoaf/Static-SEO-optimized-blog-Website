52b03cc1a3719401a4d26dc2bfd7fb1867228731
rikito_rakib <rikitoaf@gmail.com>
2024-09-30 19:02:29 +0600

    blog page design complete

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -2,7 +2,12 @@
 
 
 import { makeSource, defineDocumentType } from '@contentlayer/source-files';
+
 import readingTime from 'reading-time';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
+import GithubSlugger from "github-slugger"
 
 
 const Blog = defineDocumentType(() => ({
@@ -49,16 +54,39 @@ const Blog = defineDocumentType(() => ({
     readingTime:{
       type: 'json',
       resolve: (doc) => readingTime(doc.body.raw),
+    },
+    toc:{
+      type: "json",
+      resolve: async (doc) => {
+
+        const regulrExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const slugger = new GithubSlugger();
+        const headings = Array.from(doc.body.raw.matchAll(regulrExp)).map(({groups}) => {
+          const flag = groups?.flag;
+          const content = groups?.content;
+
+          return {
+            level: flag?.length == 1 ? "one" : flag?.length == 2 ? "two" : "three",
+            text: content,
+            slug: content ? slugger.slug(content) : undefined
+          }
+
+        })
+
+
+        return headings;
+      }
     }
-   
-   
   },
 }));
-
 
 export default makeSource({
   /* options */
   contentDirPath: 'content',
   documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug,[rehypeAutolinkHeadings, {behavior: 'append'}]],
+  },
  
 });
--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -10,7 +10,8 @@ const mdxComponent = {
 function RenderMdx({blog}) {
     const MDXContent = useMDXComponent(blog.body.code)
   return (
-    <div className='col-span-8 font-in prose'>
+    <div className='col-span-8 font-in prose porse-lg max-w-max prose-blockquote:bg-accent/20
+    prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-accent prose-blockquote:rounded-r-lg'>
         <MDXContent components={mdxComponent}/>
     </div>
   )
