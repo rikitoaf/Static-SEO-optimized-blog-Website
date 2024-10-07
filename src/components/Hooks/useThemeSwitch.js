55de508868e3bd9e1bc17b39ef27299e42b41219
rikito_rakib <rikitoaf@gmail.com>
2024-10-07 19:42:00 +0600

    seo meta data intrigated

---
+++ src/components/Hooks/useThemeSwitch.js
@@ -0,0 +1,6 @@
+"use client"
+
+export function useThemeSwitch() {
+    const preferDarkQuery = "(prefers-color-scheme: dark)";
+    const storageKey = "theme";
+}
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Alumni Website',
+    author: 'Abdul Fattah Rakib',
+    headerTitle: 'Alumni Website Blog',
+    description: 'A blog created for alumni students.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://agventurewebsite.com', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'abdulfattahrakib@gmail.com', 
+    github: 'https://github.com/rikitoaf',
+    twitter: 'https://twitter.com/code_bucks',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com/codebucks',
+    linkedin: 'https://www.linkedin.com/in/codebucks/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
