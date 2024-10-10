0756281ded21f87174f8cf218b62b54a6a22bcac
rikito_rakib <rikitoaf@gmail.com>
2024-10-10 07:25:16 +0600

    error fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -59,8 +59,8 @@ const Header = () => {
         
         >
             <Link href="/" className="mr-2">Home</Link>
-            <Link href="/about" className="mx-2">About</Link>
-            <Link href="/contact" className="mx-2">Contact</Link>
+            <Link href="/" className="mx-2">About</Link>
+            <Link href="/" className="mx-2">Contact</Link>
             <button onClick={() => setMode(mode === "light" ? "dark" : "light")  }
             className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", mode === "light" ? "bg-dark text-light" :
             "bg-light text-dark" )}
